c8c5ab6dd9847c0ec08ed49caea4608509eb65a7
Samy Alzahrani <samy@jehazy.com>
2019-03-29 14:21:53 +0300

    use microbundle to reduce bundle size

--- test/functional/index.js
+++ test/functional/index.js
@@ -5,8 +5,7 @@ import formatTests from './support/format';
 const builds = {
     'ES'      : p.resolve('lib/index.es.js'),
     'CJS'     : p.resolve('lib/index.js'),
-    'UMD-dev' : p.resolve('dist/react-intl.js'),
-    'UMD-prod': p.resolve('dist/react-intl.min.js'),
+    'UMD'     : p.resolve('lib/index.umd.js'),
 };
 
 Object.keys(builds).forEach((name) => {
